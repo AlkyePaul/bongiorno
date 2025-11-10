9832dcf9d8254bb278fce053b1a13cd93d8195d4
alkyepaul <paolo@viajaconlaia.com>
2025-11-10 10:16:41 +0100

    done

--- src/components/header/StickyOnScroll.jsx
+++ src/components/header/StickyOnScroll.jsx
@@ -10,7 +10,7 @@ export default function StickyOnScroll({children}) {
   }, []);
 
   return (
-    <div>
+    <div className='overflow-x-hidden'>
       <div className={scrolled ? 'mt-14' : 'mt-20'} />
       <header
         className={`fixed top-0 left-0 z-50 w-full transition-all duration-300 shadow-lg ${
