486c7df05aa278e5f3b6c438b5c9d322920fcd1e
alkyepaul <paolo@viajaconlaia.com>
2025-11-20 13:53:29 +0100

    loghi nuovi

--- src/components/common/LogoGrid.js
+++ src/components/common/LogoGrid.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function LogoGrid(grayscale, scale) {
 
   const logos = [
-    { src: "/img/logos/AEO.webp", alt: "AEO" },
-    { src: "/img/logos/wca.webp", alt: "WCA" },
-    { src: "/img/logos/fedespedi.webp", alt: "Fedespedi"},
-    { src: "/img/logos/iso9001.webp", alt: "ISO 9001:2015", href: PDF_RINA },
-    { src: "/img/logos/igln.webp", alt: "IGLN"}
+    { src: "/img/logos/aeo.jpg", alt: "AEO" },
+    { src: "/img/logos/wcafirst.jpg", alt: "WCA First" },
+    { src: "/img/logos/wcainter.jpg", alt: "WCA InterGlobal" },
+    { src: "/img/logos/fede.jpg", alt: "Fedespedi"},
+    { src: "/img/logos/rina.jpg", alt: "ISO 9001:2015", href: PDF_RINA },
   ];
     return (
  
@@ -16,13 +16,13 @@ export default function LogoGrid(grayscale, scale) {
           {logos.map((logo) => (
             <div
               key={logo.src}
-              className={`relative w-32 h-16 md:w-40 md:h-20 transition ${scale ? "" : "hover:scale-115"}`}
+              className={`shadow relative w-32 h-20 md:w-40 md:h-25 transition ${scale ? "" : "hover:scale-115"} bg-white p-4 rounded-lg border border-gray-200` }
             >
               <Image
                 src={logo.src}
                 alt={logo.alt}
                 fill
-                sizes="160px"
+                sizes="180px"
                 className={`object-contain ${grayscale ? "" : "grayscale hover:grayscale-0"} ${scale ? "" : "hover:scale-115"}`}
               />
               {logo.href && <Link href={logo.href} target="_blank" className="absolute inset-0" />}
